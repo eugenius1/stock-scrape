b8cec45ca6a2ba7e74ed408c87d759a26925228f
Eusebius Ngemera <eugenius1@users.noreply.github.com>
2024-09-16 02:42:43 +0200

    refactor: Tidy module structure
    - The test file is now in module
    - Use black formatter

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ logging.basicConfig(level=logging.INFO)
 
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tickers', '-t', default='tickers.csv',
-                        help='Headerless CSV with tickers and optionally the SimplyWall.St URL')
-    parser.add_argument('--output', '-o', default='output.csv',
-                        help='Output CSV for results')
+    parser.add_argument(
+        "--tickers",
+        "-t",
+        default="tickers.csv",
+        help="Headerless CSV with tickers and optionally the SimplyWall.St URL",
+    )
+    parser.add_argument(
+        "--output", "-o", default="output.csv", help="Output CSV for results"
+    )
 
     args = parser.parse_args()
     if not os.path.isfile(args.tickers):
-        raise FileNotFoundError(f'{args.tickers} is not a file.')
+        raise FileNotFoundError(f"{args.tickers} is not a file.")
     return args
 
 
@@ -29,7 +34,7 @@ def main():
 
     results = scraper.simply_wall_st(pd.read_csv(args.tickers, header=None))
 
-    logging.info(f'Writing to {args.output}')
+    logging.info(f"Writing to {args.output}")
     results.to_csv(args.output, index=False)
 
 
---
+++ scraper/test_simply_wall_st.py
@@ -0,0 +1,41 @@
+import unittest
+
+from .simply_wall_st import Patterns, _find_first, _main_url
+
+
+class TestRegexScrapers(unittest.TestCase):
+    def test_forecasted_revenue_growth(self):
+        page = "SMSN's revenue (7% per year) is forecast to grow faster than the UK market (4% per year)."
+        result = _find_first(Patterns.forecasted_revenue_growth, page)
+        self.assertEqual(result, "7%")
+
+    def test_forecasted_earnings_growth_negative(self):
+        page = "BNTX's earnings are forecast to decline over the next 3 years (-40.2% per year)."
+        result = _find_first(Patterns.forecasted_earnings_growth, page)
+        self.assertEqual(result, "-40.2%")
+
+    def test_forecasted_earnings_growth(self):
+        page = "SMSN's earnings (8.7% per year) are forecast to grow slower than the UK market (11.3% per year)."
+        result = _find_first(Patterns.forecasted_earnings_growth, page)
+        self.assertEqual(result, "8.7%")
+
+    def test_debt_to_equity(self):
+        page = "SMSN's debt to equity ratio has reduced from 7% to 4.6% over the past 5 years."
+        result = _find_first(Patterns.debt_to_equity, page)
+        self.assertEqual(result, "4.6%")
+
+    def test_debt_coverage(self):
+        page = "SMSN's debt is well covered by operating cash flow (427.4%)"
+        result = _find_first(Patterns.ocf_to_debt, page)
+        self.assertEqual(result, "427.4%")
+
+
+class TestUrlVerifier(unittest.TestCase):
+    def test_(self):
+        news_url = "https://simplywall.st/stocks/us/semiconductors/nasdaq-xlnx/xilinx/news/is-it-too-late-to-consider-buying-xilinx-inc-nasdaqxlnx"
+        main_url = "https://simplywall.st/stocks/us/semiconductors/nasdaq-xlnx/xilinx"
+        self.assertEqual(main_url, _main_url(news_url))
+
+
+if __name__ == "__main__":
+    unittest.main()
